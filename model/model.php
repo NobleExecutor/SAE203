05bd0ec532aa7ed339a944b78dac07d01d6feaba
NobleExecutor <brakes_juniors.0t@icloud.com>
2026-04-28 11:35:00 +0200

    Changed more comments

--- model/model.php
+++ model/model.php
@@ -1,13 +1,4 @@
 <?php
-  // ****** ACCES AUX DONNEES ******
-
-/*******************************************************
-  Retourne la liste des clients 
-    Entrée : 
-  
-    Retour : 
-      [array] : Tableau associatif contenant la liste des clients
- *******************************************************/
 function connexionBDD()
 {
   try // Connexion à la base de données
@@ -18,14 +9,4 @@ function connexionBDD()
     die('Erreur connexion MySQL : ' . $err->getMessage());
   }
   return $database;
-}
-
-function lectureBDD($requete)
-{
-  $bdd = connexionBDD(); // Connexion à la base de données
-  $reponse = $bdd->query($requete); // Envoi de la requête SQL
-  // Lecture de toutes les lignes de la réponse dans un tableau associatif
-  $tableau = $reponse->fetchAll(PDO::FETCH_ASSOC);
-  $bdd = null; // Fin de la connexion
-  return $tableau; // Renvoi du tableau associatif
 }
