0ba0af62a291a808be85b5b520160ea86812abde
NobleExecutor <brakes_juniors.0t@icloud.com>
2026-04-29 21:31:39 +0200

    Renamed style.css to style-index.css

--- view/view.php
+++ view/view.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/fontface.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style-index.css">
     <title>NGE Wiki</title>
 </head>
 
