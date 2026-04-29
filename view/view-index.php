e7ad6122d07a7d201310faa66b825c4f0079a4b2
NobleExecutor <brakes_juniors.0t@icloud.com>
2026-04-29 23:16:14 +0200

    Added favicon

--- view/view-index.php
+++ view/view-index.php
@@ -10,6 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/fontface.css">
     <link rel="stylesheet" href="css/style-index.css">
     <title>NGE Wiki</title>
--- view/view-units.php
+++ view/view-units.php
@@ -7,6 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="css/fontface.css">
     <link rel="stylesheet" href="css/style-global.css">
     <title>NGE Wiki - Unités EVA</title>
